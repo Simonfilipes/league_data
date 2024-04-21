1b84fcd69ce1caa1c73be31fbe652580d76c4e7e
BocchiProgram <simonfiliper@gmail.com>
2024-04-21 12:58:26 -0300

    tradutor na pag_apres

--- pag_apres.php
+++ pag_apres.php
@@ -10,153 +10,153 @@
 </head>
 
 <body>
-  <?php include('header.php')?>
+  <?php include('header.php') ?>
 
   <!-- BOAS VINDAS -->
-    <section>
-      <div class="div_apresentacao">
-        <div class="div_apresentacao_txt">
-          <h1>BEM VINDO AO <span id="spam_1">LEAGUE DATA</span><br> O <span>LEAGUE OF LEGENDS</span> NA <br>PALMA DE SUA
-            MÃO</h1>
+  <section>
+    <div class="div_apresentacao">
+      <div class="div_apresentacao_txt">
+        <h1>BEM VINDO AO <span id="spam_1">LEAGUE DATA</span><br> O <span>LEAGUE OF LEGENDS</span> NA <br>PALMA DE SUA
+          MÃO</h1>
 
-          <button class="button"><span class="button_lg"><span class="button_sl"></span><span class="button_text" id="button_color"><a id=button_a href="#titulo">Funcionalidades do Site</a></span></span></button>
-        </div>
-        <div class="div_apresentacao_img">
-          <img src="img/img_1.png" alt="img_1" id="img_1">
-        </div>
-
+        <button class="button"><span class="button_lg"><span class="button_sl"></span><span class="button_text" id="button_color"><a id=button_a href="#titulo">Funcionalidades do Site</a></span></span></button>
       </div>
-    </section>
+      <div class="div_apresentacao_img">
+        <img src="img/img_1.png" alt="img_1" id="img_1">
+      </div>
 
-    <!-- BOAS VINDAS -->
-
-    <div id="titulo"></div>
-
-    <div class="titulo">
-      <h2>Funções do Site</h2>
     </div>
+  </section>
+
+  <!-- BOAS VINDAS -->
+
+  <div id="titulo"></div>
+
+  <div class="titulo">
+    <h2>Funções do Site</h2>
+  </div>
 
 
-    <!-- CARDS -->
+  <!-- CARDS -->
 
-    <section class="section_cards">
-      <div class="div_cartas">
+  <section class="section_cards">
+    <div class="div_cartas">
 
-        <div class="container noselect">
-          <div class="canvas">
-            <div class="tracker tr-1"></div>
-            <div class="tracker tr-2"></div>
-            <div class="tracker tr-3"></div>
-            <div class="tracker tr-4"></div>
-            <div class="tracker tr-5"></div>
-            <div class="tracker tr-6"></div>
-            <div class="tracker tr-7"></div>
-            <div class="tracker tr-8"></div>
-            <div class="tracker tr-9"></div>
-            <div class="tracker tr-10"></div>
-            <div class="tracker tr-11"></div>
-            <div class="tracker tr-12"></div>
-            <div class="tracker tr-13"></div>
-            <div class="tracker tr-14"></div>
-            <div class="tracker tr-15"></div>
-            <div class="tracker tr-16"></div>
-            <div class="tracker tr-17"></div>
-            <div class="tracker tr-18"></div>
-            <div class="tracker tr-19"></div>
-            <div class="tracker tr-20"></div>
-            <div class="tracker tr-21"></div>
-            <div class="tracker tr-22"></div>
-            <div class="tracker tr-23"></div>
-            <div class="tracker tr-24"></div>
-            <div class="tracker tr-25"></div>
-            <div id="card">
-              <p id="prompt"><i class="bi bi-arrow-down"></i></p>
-              <div class="title">Amostra de todos <br>os campeões de <br>League Of Legends</div>
-              <div class="subtitle">
-                Champs
-              </div>
-
+      <div class="container noselect">
+        <div class="canvas">
+          <div class="tracker tr-1"></div>
+          <div class="tracker tr-2"></div>
+          <div class="tracker tr-3"></div>
+          <div class="tracker tr-4"></div>
+          <div class="tracker tr-5"></div>
+          <div class="tracker tr-6"></div>
+          <div class="tracker tr-7"></div>
+          <div class="tracker tr-8"></div>
+          <div class="tracker tr-9"></div>
+          <div class="tracker tr-10"></div>
+          <div class="tracker tr-11"></div>
+          <div class="tracker tr-12"></div>
+          <div class="tracker tr-13"></div>
+          <div class="tracker tr-14"></div>
+          <div class="tracker tr-15"></div>
+          <div class="tracker tr-16"></div>
+          <div class="tracker tr-17"></div>
+          <div class="tracker tr-18"></div>
+          <div class="tracker tr-19"></div>
+          <div class="tracker tr-20"></div>
+          <div class="tracker tr-21"></div>
+          <div class="tracker tr-22"></div>
+          <div class="tracker tr-23"></div>
+          <div class="tracker tr-24"></div>
+          <div class="tracker tr-25"></div>
+          <div id="card">
+            <p id="prompt"><i class="bi bi-arrow-down"></i></p>
+            <div class="title">Amostra de todos <br>os campeões de <br>League Of Legends</div>
+            <div class="subtitle">
+              Champs
             </div>
+
           </div>
         </div>
-
-        <div class="container noselect">
-          <div class="canvas">
-            <div class="tracker tr-1"></div>
-            <div class="tracker tr-2"></div>
-            <div class="tracker tr-3"></div>
-            <div class="tracker tr-4"></div>
-            <div class="tracker tr-5"></div>
-            <div class="tracker tr-6"></div>
-            <div class="tracker tr-7"></div>
-            <div class="tracker tr-8"></div>
-            <div class="tracker tr-9"></div>
-            <div class="tracker tr-10"></div>
-            <div class="tracker tr-11"></div>
-            <div class="tracker tr-12"></div>
-            <div class="tracker tr-13"></div>
-            <div class="tracker tr-14"></div>
-            <div class="tracker tr-15"></div>
-            <div class="tracker tr-16"></div>
-            <div class="tracker tr-17"></div>
-            <div class="tracker tr-18"></div>
-            <div class="tracker tr-19"></div>
-            <div class="tracker tr-20"></div>
-            <div class="tracker tr-21"></div>
-            <div class="tracker tr-22"></div>
-            <div class="tracker tr-23"></div>
-            <div class="tracker tr-24"></div>
-            <div class="tracker tr-25"></div>
-            <div id="card">
-              <p id="prompt"><i class="bi bi-arrow-down"></i></p>
-              <div class="title">Database de todos<br> os campeões e<br> invocadores com <br>suas informações</div>
-              <div class="subtitle">
-                League Data
-              </div>
-
-            </div>
-          </div>
-        </div>
-
-        <div class="container noselect">
-          <div class="canvas">
-            <div class="tracker tr-1"></div>
-            <div class="tracker tr-2"></div>
-            <div class="tracker tr-3"></div>
-            <div class="tracker tr-4"></div>
-            <div class="tracker tr-5"></div>
-            <div class="tracker tr-6"></div>
-            <div class="tracker tr-7"></div>
-            <div class="tracker tr-8"></div>
-            <div class="tracker tr-9"></div>
-            <div class="tracker tr-10"></div>
-            <div class="tracker tr-11"></div>
-            <div class="tracker tr-12"></div>
-            <div class="tracker tr-13"></div>
-            <div class="tracker tr-14"></div>
-            <div class="tracker tr-15"></div>
-            <div class="tracker tr-16"></div>
-            <div class="tracker tr-17"></div>
-            <div class="tracker tr-18"></div>
-            <div class="tracker tr-19"></div>
-            <div class="tracker tr-20"></div>
-            <div class="tracker tr-21"></div>
-            <div class="tracker tr-22"></div>
-            <div class="tracker tr-23"></div>
-            <div class="tracker tr-24"></div>
-            <div class="tracker tr-25"></div>
-            <div id="card">
-              <p id="prompt"><i class="bi bi-arrow-down"></i></p>
-              <div class="title">Três Jogos de<br> adivinhação, teste<br> sua memoria e seu<br> conhecimento no LoL</div>
-              <div class="subtitle">
-                Loldle
-              </div>
-
-            </div>
-          </div>
-        </div>
-
       </div>
+
+      <div class="container noselect">
+        <div class="canvas">
+          <div class="tracker tr-1"></div>
+          <div class="tracker tr-2"></div>
+          <div class="tracker tr-3"></div>
+          <div class="tracker tr-4"></div>
+          <div class="tracker tr-5"></div>
+          <div class="tracker tr-6"></div>
+          <div class="tracker tr-7"></div>
+          <div class="tracker tr-8"></div>
+          <div class="tracker tr-9"></div>
+          <div class="tracker tr-10"></div>
+          <div class="tracker tr-11"></div>
+          <div class="tracker tr-12"></div>
+          <div class="tracker tr-13"></div>
+          <div class="tracker tr-14"></div>
+          <div class="tracker tr-15"></div>
+          <div class="tracker tr-16"></div>
+          <div class="tracker tr-17"></div>
+          <div class="tracker tr-18"></div>
+          <div class="tracker tr-19"></div>
+          <div class="tracker tr-20"></div>
+          <div class="tracker tr-21"></div>
+          <div class="tracker tr-22"></div>
+          <div class="tracker tr-23"></div>
+          <div class="tracker tr-24"></div>
+          <div class="tracker tr-25"></div>
+          <div id="card">
+            <p id="prompt"><i class="bi bi-arrow-down"></i></p>
+            <div class="title">Database de todos<br> os campeões e<br> invocadores com <br>suas informações</div>
+            <div class="subtitle">
+              League Data
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="container noselect">
+        <div class="canvas">
+          <div class="tracker tr-1"></div>
+          <div class="tracker tr-2"></div>
+          <div class="tracker tr-3"></div>
+          <div class="tracker tr-4"></div>
+          <div class="tracker tr-5"></div>
+          <div class="tracker tr-6"></div>
+          <div class="tracker tr-7"></div>
+          <div class="tracker tr-8"></div>
+          <div class="tracker tr-9"></div>
+          <div class="tracker tr-10"></div>
+          <div class="tracker tr-11"></div>
+          <div class="tracker tr-12"></div>
+          <div class="tracker tr-13"></div>
+          <div class="tracker tr-14"></div>
+          <div class="tracker tr-15"></div>
+          <div class="tracker tr-16"></div>
+          <div class="tracker tr-17"></div>
+          <div class="tracker tr-18"></div>
+          <div class="tracker tr-19"></div>
+          <div class="tracker tr-20"></div>
+          <div class="tracker tr-21"></div>
+          <div class="tracker tr-22"></div>
+          <div class="tracker tr-23"></div>
+          <div class="tracker tr-24"></div>
+          <div class="tracker tr-25"></div>
+          <div id="card">
+            <p id="prompt"><i class="bi bi-arrow-down"></i></p>
+            <div class="title">Três Jogos de<br> adivinhação, teste<br> sua memoria e seu<br> conhecimento no LoL</div>
+            <div class="subtitle">
+              Loldle
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
 
 
 
@@ -164,6 +164,27 @@
 
     <!-- CARDS -->
   </section>
+
+  <div class="gtranslate_wrapper"></div>
+  <script>
+    window.gtranslateSettings = {
+      "default_language": "pt",
+      "native_language_names": true,
+      "languages": ["pt", "en", "ja", "ko", "it", "es"],
+      "globe_color": "#66aaff",
+      "wrapper_selector": ".gtranslate_wrapper",
+      "flag_size": 16,
+      "horizontal_position": "left",
+      "vertical_position": "bottom",
+      "alt_flags": {
+        "en": "usa",
+        "pt": "brazil"
+      },
+      "globe_size": 40
+    }
+  </script>
+  <script src="https://cdn.gtranslate.net/widgets/latest/globe.js" defer></script>
+
 </body>
 
 </html>
