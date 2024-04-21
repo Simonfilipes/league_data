de90c4d4eab6ed10d256a62808c998f45e2a8fb1
BocchiProgram <simonfiliper@gmail.com>
2024-04-21 12:56:04 -0300

    Header e Tradutor

--- header.php
+++ header.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/header.css">
-        <link rel="stylesheet" href="css/responsive.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/search_bar.css">
-        <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-    </head>
-    <body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/search_bar.css">
+    <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+</head>
+
+<body>
     <script type="text/javascript">
         function googleTranslateElementInit() {
             new google.translate.TranslateElement({
@@ -18,45 +20,48 @@
                 layout: google.translate.TranslateElement.InlineLayout.NULL
             }, 'google_translate_element');
         }
-        
+
         function traduzirParaIdioma(idioma) {
             document.documentElement.lang = idioma;
         }
     </script>
 
-        <header>
-            <div class="header_box">
-                <div class="header_box_div">
-                    <div class="div_logo">
-                        <a href="index.php"><img src="img/logo.jpg" alt="logo" id="logo"></a>
-                    </div>
-
-                    <div class="nav_div">
-                        <nav>
-                            <a href="champions.php">Champs</a>
-                            <a href="#">League Data</a>
-                            <a href="#">Loldle</a>
-                        </nav>
-                    </div>
-
-                    <script type="text/javascript">
-                function tornarTransparente() {
-                    document.getElementById('translate_icon').style.opacity = '0';
-                }
-                function tornarVisivelNovamente() {
-                    document.getElementById('translate_icon').style.opacity = '1';
-                }
-            </script>
-
-                    <div class="button_header" onmouseover="tornarTransparente()" onmouseout="tornarVisivelNovamente()">
-                    
-                    <div id="google_translate_element"></div>
-                        <i class="bi bi-translate" id="translate_icon"></i>
-                    </div>
-
+    <header>
+        <div class="header_box">
+            <div class="header_box_div">
+                <div class="div_logo">
+                    <a href="index.php"><img src="img/logo.jpg" alt="logo" id="logo"></a>
                 </div>
-            </div>
-        </header>
-    </body>
-</html>
 
+                <div class="nav_div">
+                    <nav>
+                        <a href="champions.php">Champs</a>
+                        -
+                        <a href="#">League Data</a>
+                        -
+                        <a href="#">Loldle</a>
+                    </nav>
+                </div>
+
+                <script type="text/javascript">
+                    function tornarTransparente() {
+                        document.getElementById('translate_icon').style.opacity = '0';
+                    }
+
+                    function tornarVisivelNovamente() {
+                        document.getElementById('translate_icon').style.opacity = '1';
+                    }
+                </script>
+
+                <a href="pag_apres.php" id="feedback_a">
+                    <div class="button_header" onmouseover="tornarTransparente()" onmouseout="tornarVisivelNovamente()">
+                        <i class="bi bi-star"></i>
+                    </div>
+                </a>
+
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-  <?php include('header.php')?>
+  <?php include('header.php') ?>
   <div class=" box">
     <div class="banner">
       <img src="img\banner.png" alt="" srcset="">
@@ -23,10 +23,31 @@
       </select>
       <input type="text" placeholder='Pesquise pelo campeão ou pelo seu perfil de invocador' id="search_input">
       <svg viewBox="0 0 20 20" aria-hidden="true" style='max-height: 40px;' id="svg">
-            <path d="M16.72 17.78a.75.75 0 1 0 1.06-1.06l-1.06 1.06ZM9 14.5A5.5 5.5 0 0 1 3.5 9H2a7 7 0 0 0 7 7v-1.5ZM3.5 9A5.5 5.5 0 0 1 9 3.5V2a7 7 0 0 0-7 7h1.5ZM9 3.5A5.5 5.5 0 0 1 14.5 9H16a7 7 0 0 0-7-7v1.5Zm3.89 10.45 3.83 3.83 1.06-1.06-3.83-3.83-1.06 1.06ZM14.5 9a5.48 5.48 0 0 1-1.61 3.89l1.06 1.06A6.98 6.98 0 0 0 16 9h-1.5Zm-1.61 3.89A5.48 5.48 0 0 1 9 14.5V16a6.98 6.98 0 0 0 4.95-2.05l-1.06-1.06Z"></path>
+        <path d="M16.72 17.78a.75.75 0 1 0 1.06-1.06l-1.06 1.06ZM9 14.5A5.5 5.5 0 0 1 3.5 9H2a7 7 0 0 0 7 7v-1.5ZM3.5 9A5.5 5.5 0 0 1 9 3.5V2a7 7 0 0 0-7 7h1.5ZM9 3.5A5.5 5.5 0 0 1 14.5 9H16a7 7 0 0 0-7-7v1.5Zm3.89 10.45 3.83 3.83 1.06-1.06-3.83-3.83-1.06 1.06ZM14.5 9a5.48 5.48 0 0 1-1.61 3.89l1.06 1.06A6.98 6.98 0 0 0 16 9h-1.5Zm-1.61 3.89A5.48 5.48 0 0 1 9 14.5V16a6.98 6.98 0 0 0 4.95-2.05l-1.06-1.06Z"></path>
       </svg>
     </div>
   </div>
+
+  <div class="gtranslate_wrapper"></div>
+
+  <script>
+    window.gtranslateSettings = {
+      "default_language": "pt",
+      "native_language_names": true,
+      "languages": ["pt", "en", "ja", "ko", "it", "es"],
+      "globe_color": "#66aaff",
+      "wrapper_selector": ".gtranslate_wrapper",
+      "flag_size": 24,
+      "horizontal_position": "left",
+      "vertical_position": "bottom",
+      "alt_flags": {
+        "en": "usa",
+        "pt": "brazil"
+      }
+    }
+  </script>
+
+  <script src="https://cdn.gtranslate.net/widgets/latest/globe.js" defer></script>
 
 </body>
 
