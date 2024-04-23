5108bd4a6d35da4c1a203bc21a77372a46ee30cd
Simon Filipe <167369596+Simonfilipes@users.noreply.github.com>
2024-04-22 21:30:48 -0300

    algumas coisas

--- champion.php
+++ champion.php
@@ -9,37 +9,65 @@
 </div>
 
 <div class="champ_box">
-    <div>
-        <div class="champ_img">
-            <img src="img\sona.jpg" alt="">
+        <div class="skills_and_skins">
+            <div class="champ_img">
+                <img src="img\sona.jpg" alt="" id="img_champs">
+            </div>
+
+            <h2>Habilidades</h2>
+
+            <div class="skills">
+                <div class="skill">
+                    <div class="div_margin">
+                        <img src="img\logo.jpg" alt="" srcset="" id="img_skill">
+                        <div class="div_p">
+                        <p id="p_id">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="div_margin">
+                        <img src="img\logo.jpg" alt="" srcset="" id="img_skill">
+                        <div class="div_p">
+                        <p id="p_id">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="div_margin">
+                        <img src="img\logo.jpg" alt="" srcset="" id="img_skill">
+                        <div class="div_p">
+                        <p id="p_id">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="div_margin">
+                        <img src="img\logo.jpg" alt="" srcset="" id="img_skill">
+                        <div class="div_p">
+                        <p id="p_id">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="div_margin">
+                        <img src="img\logo.jpg" alt="" srcset="" id="img_skill">
+                        <div class="div_p">
+                        <p id="p_id">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    
 
-        <div class="skills">
-            <div class="skill1">
-                <img src="img\logo.jpg" alt="" srcset="">
-            </div>
-            <div class="skill2">
-                <img src="img\logo.jpg" alt="" srcset="">
-            </div>
-            <div class="skill3">
-                <img src="img\logo.jpg" alt="" srcset="">
-            </div>
-            <div class="skill4">
-                <img src="img\logo.jpg" alt="" srcset="">
-            </div>
-            <div class="skill5">
-                <img src="img\logo.jpg" alt="" srcset="">
-            </div>
+        <div class="text_details">
+            <h2>Informações</h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh, vitae mollis urna molestie id. Integer ut ex nec tellus laoreet luctus. Etiam aliquet viverra libero sit amet dignissim. Sed tincidunt aliquet ligula non ultrices. Aenean eget massa sem. Donec tincidunt suscipit risus, sed suscipit lectus sagittis a. Suspendisse in ultrices dolor, quis hendrerit sem.
+
+            Nulla libero ante, porttitor ut ex id, egestas bibendum ante. Maecenas nec quam ut est molestie auctor ac id est. Nunc ac metus tincidunt, imperdiet libero eget, condimentum mauris. Proin consequat cursus augue in vehicula. Cras finibus efficitur venenatis. Duis id purus non justo pulvinar eleifend. Nulla facilisi. Curabitur interdum vel diam a tempor. Morbi sagittis euismod rhoncus. Pellentesque vitae euismod tortor, ac scelerisque tellus.
+            </p>
         </div>
-    </div>
-
-    <div class="text_details">
-        <h2>Informações</h2>
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue sodales nibh, vitae mollis urna molestie id. Integer ut ex nec tellus laoreet luctus. Etiam aliquet viverra libero sit amet dignissim. Sed tincidunt aliquet ligula non ultrices. Aenean eget massa sem. Donec tincidunt suscipit risus, sed suscipit lectus sagittis a. Suspendisse in ultrices dolor, quis hendrerit sem.
-
-        Nulla libero ante, porttitor ut ex id, egestas bibendum ante. Maecenas nec quam ut est molestie auctor ac id est. Nunc ac metus tincidunt, imperdiet libero eget, condimentum mauris. Proin consequat cursus augue in vehicula. Cras finibus efficitur venenatis. Duis id purus non justo pulvinar eleifend. Nulla facilisi. Curabitur interdum vel diam a tempor. Morbi sagittis euismod rhoncus. Pellentesque vitae euismod tortor, ac scelerisque tellus.
-        </p>
-    </div>
 </div>
 
