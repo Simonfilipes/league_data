9b97ec43256675fb7ccf69ef2cfdb02c86ccb761
vouto <68758638+andradavic@users.noreply.github.com>
2024-04-24 01:01:25 -0300

    mudada a estrutura de requests no php e parametro passado em champions.php

--- request/getChampions.php
+++ request/getChampions.php
@@ -1,35 +1,39 @@
 <?php
-    function champion() {
-        $url = 'https://ddragon.leagueoflegends.com/cdn/14.8.1/data/en_US/champion.json';
-        $response_json = file_get_contents($url);
-        $data = json_decode($response_json, true);
-        $dados_desejados = $data['data'];
-        $champions = [];
+    class API{
+        public $champions_url = "https://ddragon.leagueoflegends.com/cdn/14.8.1/data/en_US/champion.json";
+        
+        public $champion_url = "https://ddragon.leagueoflegends.com/cdn/14.8.1/data/en_US/champion/%d.json";
 
-        foreach ($dados_desejados as $db_Champion) {
-            $champion = [
-                'champName' => $db_Champion['name'],
-                'imgUrl' => "https://ddragon.leagueoflegends.com/cdn/14.8.1/img/champion/$db_Champion[id].png"
-            ];
+        public $splash_url = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/%s_%d.jpg";
+        
 
-            $champions[] = $champion;
+
+        function champions() {
+            $response_json = file_get_contents($this->champions_url);
+            $data = json_decode($response_json, true);
+            $dados_desejados = $data['data'];
+            $champions = [];
+        
+            foreach ($dados_desejados as $db_Champion) {
+                $champion = [
+                    'champName' => $db_Champion['name'],
+                    'imgUrl' => "https://ddragon.leagueoflegends.com/cdn/14.8.1/img/champion/$db_Champion[id].png",
+                    'id' => $db_Champion['id']
+                ];
+        
+                $champions[] = $champion;
+            }
+            return $champions;
         }
-        return $champions;
-    }
-
-
-    function skins() {
-        $url = 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg';
-        $response_json = file_get_contents($url);
-        $data = json_decode($response_json, true);
-
-       
-
-        for ($i = 0; $i < 5; $i++) { 
-            $base_url = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_$i.jpg";
-            echo "<img src='$base_url' alt=''>" . "<br>";
-
+        
+    
+        function skins($champ) {
+            for ($i = 0; $i < 5; $i++) { 
+                $url = sprintf($this->splash_url, $champ, $i); // Não dá para usar antes de ter um função que retorne quantas skins tem o campeão
+                echo "<img src='$url' alt=''>" . "<br>";
+            }
         }
+        
     }
-    skins();
+    
 ?>
