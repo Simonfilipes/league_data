b17635f5d5a6e00db69bcff58084a26d13db3595
vouto <68758638+andradavic@users.noreply.github.com>
2024-04-27 22:11:00 -0300

    style summoner

--- summoner.php
+++ summoner.php
@@ -9,7 +9,7 @@
     <div class="summonerprofile">
         <div class='profileArea1'>
             <div  class='profileArea2'>
-                <p>gold1</p>
+                <p>gold1 2022</p>
                 <p>gold1</p>
                 <p>gold1</p>
                 <p>gold1</p>
@@ -21,8 +21,8 @@
                     <p>302</p>
                 </div>
                 <div class="infoProfile">
-                    <p>LEague perfil #br1</p>
-                    <p>rank 10000k</p>
+                    <p id='nickname'>love darkless #br1</p>
+                    <p id='rank'>rank 10000k</p>
                     <button>update!</button>
                 </div>
             </div>
@@ -31,15 +31,15 @@
         <div class="maestrias">
 
             <div>
-                Ricardo
+                <div class="champion-image-container champion-image-container--square"><img src="https://opgg-static.akamaized.net/images/champion_mastery/flag_07.png?image=q_auto,f_webp,w_80&amp;v=1714040186300" width="40" alt="Ekko" class="champion-level"><img src="https://opgg-static.akamaized.net/meta/images/lol/14.8.1/champion/Ekko.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto,f_webp,w_160,h_160&amp;v=1714040186300" width="40" height="40" alt="Ekko" class="champion-image champion-image--square"></div>
             </div>
 
             <div>
-                Ricardo
+                <div class="champion-image-container champion-image-container--square"><img src="https://opgg-static.akamaized.net/images/champion_mastery/flag_07.png?image=q_auto,f_webp,w_80&amp;v=1714040186300" width="40" alt="Ekko" class="champion-level"><img src="https://opgg-static.akamaized.net/meta/images/lol/14.8.1/champion/Ekko.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto,f_webp,w_160,h_160&amp;v=1714040186300" width="40" height="40" alt="Ekko" class="champion-image champion-image--square"></div>
             </div>
 
             <div>
-                Ricardo
+                <div class="champion-image-container champion-image-container--square"><img src="https://opgg-static.akamaized.net/images/champion_mastery/flag_07.png?image=q_auto,f_webp,w_80&amp;v=1714040186300" width="40" alt="Ekko" class="champion-level"><img src="https://opgg-static.akamaized.net/meta/images/lol/14.8.1/champion/Ekko.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto,f_webp,w_160,h_160&amp;v=1714040186300" width="40" height="40" alt="Ekko" class="champion-image champion-image--square"></div>
             </div>
 
         </div>
